4e41f73fea51b23f453a268c63233ac8b38bceed
Bartłomiej Wilczek <bartlomiejwilczek@o2.pl>
2019-04-12 21:42:01 +0200

    no translate slider on desktop

--- elements/block-slider.php
+++ elements/block-slider.php
@@ -18,10 +18,14 @@ if ($query->have_posts()) :
 	echo '<div class="block-slider__wrapper"><div class="slider--container"><div class="slider--view"><ul>';
     while ($query->have_posts()) : $query->the_post();
         $image = get_field('slider_image');
-		echo '<li class="slider--view__slides' . ($i === 1 ? ' is-active' : '' ) . '"><img src="'.$image.'"/><p class="">'.get_the_content().'</p></li>';
-        $i++;
-    
-           
+        ?>
+        
+        <li class="slider--view__slides <?php echo ($i === 1 ? ' is-active' : '' ); ?> ">
+        <img src="<?php echo $image ?>"/>
+        <p class=""><?php get_the_content(); ?></p>
+        </li>
+        <?php
+        $i++;      
 	endwhile;
 	echo '</ul></div><div class="slider--arrows"><span class="arrow slider--arrows__left">&#x3c;</span><span class="arrow slider--arrows__right">&#x3e;</span></div></div></div>';
 endif;
