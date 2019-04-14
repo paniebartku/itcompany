084bfbc12688f92c729fd6c5693f191576887744
Bartłomiej Wilczek <bartlomiejwilczek@o2.pl>
2019-04-14 20:40:12 +0200

    dots to slider

--- elements/block-slider.php
+++ elements/block-slider.php
@@ -14,19 +14,36 @@ $args = array(
 $query = new WP_Query( $args );
 if ($query->have_posts()) :
         $i = 1;
-         
-	echo '<div class="block-slider__wrapper"><div class="slider--container"><div class="slider--view"><ul>';
+         ?>
+        <div class="block-slider__wrapper">
+            <div class="slider--container">
+                <div class="slider--view">
+                    <ul>
+    <?php
     while ($query->have_posts()) : $query->the_post();
         $image = get_field('slider_image');
         ?>
-        
-        <li class="slider--view__slides <?php echo ($i === 1 ? ' is-active' : '' ); ?> ">
-        <img src="<?php echo $image ?>"/>
-        <p class=""><?php get_the_content(); ?></p>
-        </li>
+                        <li class="slider--view__slides <?php echo ($i === 1 ? ' is-active' : '' ); ?> ">
+                            <img src="<?php echo $image ?>"/>
+                            <p class=""><?php get_the_content(); ?></p>
+                        </li>
+       
         <?php
-        $i++;      
-	endwhile;
+        $i++; 
+        ?>
+         
+        <?php   
+    endwhile; ?>
+    <div class="dots">
+        <?php
+        $i = 1;
+    while ($query->have_posts()) : $query->the_post(); ?>
+    
+            <span class=<?php echo ($i === 1 ? ' is-active' : '' ); ?>></span>
+       <?php
+       $i++; 
+    endwhile;  ?>
+    </div>   <?php
 	echo '</ul></div><div class="slider--arrows"><span class="arrow slider--arrows__left">&#x3c;</span><span class="arrow slider--arrows__right">&#x3e;</span></div></div></div>';
 endif;
 wp_reset_postdata();
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,26 +1,29 @@
-const slider = (() => {
 
+const slider = (() => {
+    
 
 const sliderViewSlides = [...document.querySelectorAll('.slider--view__slides')],
     arrowLeft = document.querySelector('.slider--arrows__left'),
     arrowRight = document.querySelector('.slider--arrows__right'),
     sliderLength = sliderViewSlides.length;
-    const time = 5000;
-
+    const time = 7000;
+    const dots = [...document.querySelectorAll('.dots span')];
+    let active = 0;
 const slideMe = (sliderViewItems, isActiveItem) => {
  
     isActiveItem.classList.remove('is-active');
   
     sliderViewItems.classList.add('is-active');
-   
+  
     //sliderView.setAttribute('style', 'transform:translateX(-' + sliderViewItems.offsetLeft + 'px)');
 }
 
-// if (window.matchMedia("(max-width: 700px)").matches) {
-//     alert("The viewport is less than, or equal to, 700 pixels wide")
-//   } else {
-//    alert(" The viewport is greater than 700 pixels wide")
-//   }
+const changeDot = () => {
+    const activeDot = dots.findIndex(dot => dot.classList.contains('is-active'));
+    dots[activeDot].classList.remove('is-active');
+    dots[active].classList.add('is-active');
+}
+
 
 const beforeSliding = i => {
     let isActiveItem = document.querySelector('.slider--view__slides.is-active'),
@@ -39,33 +42,66 @@ const beforeSliding = i => {
     slideMe(sliderViewItems, isActiveItem);
 }
 const changeSlide = () => {
+    active++;
     beforeSliding(1);
-   }
-   let indexInterval = setInterval(changeSlide, time);
-
-   let active = 0;
-
-   const changeActive = () => {
     if(active === sliderLength){
         active = 0;
     } else if (active < 0){
         active = sliderLength - 1;
     }
+    changeDot();
+   
+   
+   }
+   let indexInterval = setInterval(changeSlide, time);
+
+  
+
+   const changeActive = () => {
+    active++
+    if(active === sliderLength){
+        active = 0;
+    } else if (active < 0){
+        active = sliderLength - 1;
+    }
+
+    changeDot();
+    indexInterval = setInterval(changeSlide, time);
+}
+const changeActiveBack = () => {
+    active--
+    if(active === sliderLength){
+        active = 0;
+    } else if (active < 0){
+        active = sliderLength - 1;
+    }
+
+    changeDot();
     indexInterval = setInterval(changeSlide, time);
 }
 
 
 arrowRight.addEventListener('click', () => {
     clearInterval(indexInterval);
+    changeDot();
     beforeSliding(1);
     changeActive();
 });
 arrowLeft.addEventListener('click', () => {
     clearInterval(indexInterval);
+    changeDot();
     beforeSliding(0);
-    changeActive();
+    changeActiveBack();
 });
 
 })(); 
+
+
+// if (window.matchMedia("(max-width: 700px)").matches) {
+//     alert("The viewport is less than, or equal to, 700 pixels wide")
+//   } else {
+//    alert(" The viewport is greater than 700 pixels wide")
+//   }
+
 
 export default slider;
