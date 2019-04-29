5333cb2094f6f77f7f6e823882914e7cd4a0ed34
Bartłomiej Wilczek <bartlomiejwilczek@o2.pl>
2019-04-29 22:12:38 +0200

    script same height - window.innerWidth condition

--- src/js/sameHeight.js
+++ src/js/sameHeight.js
@@ -2,35 +2,45 @@ const sameHeight = (() => {
     
 	const blockquotes = [...document.querySelectorAll('blockquote')];
     let tallest = 0;
-    
- 	for(let i = 0; i < blockquotes.length; i++){
-		
-		let element = blockquotes[i];
-		let elementHeight = element.offsetHeight;
-		tallest = (elementHeight>tallest ? elementHeight : tallest); 
-		
-	 }
-    for(let i = 0; i < blockquotes.length; i++){
-        blockquotes[i].style.height = tallest + "px";
-	}
 
-
+			for(let i = 0; i < blockquotes.length; i++){
+				
+				let element = blockquotes[i];
+				let elementHeight = element.offsetHeight;
+				tallest = (elementHeight>tallest ? elementHeight : tallest); 
+				
+			}
+			for(let i = 0; i < blockquotes.length; i++){
+				blockquotes[i].style.height = tallest + "px";
+			}
+		
+	
 
 	window.addEventListener('resize', ()=>{
 	
-		for(let i = 0; i < blockquotes.length; i++){
-			
-			let element = blockquotes[i];
-			element.style.height = '';
-			let elementHeight = element.offsetHeight;
-			
-			console.log(elementHeight);
-			tallest = (elementHeight>tallest ? elementHeight : tallest); 
-			
-			blockquotes[i].style.height = tallest + "px";
-			console.log(elementHeight);
-		 }
+		console.log(window.innerWidth);
 	
+		if(window.innerWidth >= 991 ){
+			for(let i = 0; i < blockquotes.length; i++){
+				
+				let element = blockquotes[i];
+				element.style.height = '';
+				let elementHeight = element.offsetHeight;
+				
+				//console.log(elementHeight);
+				tallest = (elementHeight>tallest ? elementHeight : tallest); 
+				
+				blockquotes[i].style.height = tallest + "px";
+				//console.log(elementHeight);
+			}
+		} else {
+			
+			for(let i = 0; i < blockquotes.length; i++){
+				blockquotes[i].style.height = '';
+			}
+		
+	
+		}
 	})
 })();
  
