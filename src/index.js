37241cdd1a65cb7ad883771322d1f4c206567036
Bartłomiej Wilczek <bartlomiejwilczek@o2.pl>
2019-05-06 22:31:37 +0200

    import glide.js to project

--- src/index.js
+++ src/index.js
@@ -9,8 +9,23 @@ import slider from "./js/slider.js";
 import scrollToTop from "./js/scrollToTop.js";
 import sameHeight from "./js/sameHeight.js";
 import addClasses from "./js/addClasses";
+import Glide from '@glidejs/glide'
+
+new Glide('.glide', {
+  type: 'carousel',
+  hoverpause: false,
+  autoplay: false, // set to true
+  perView: 3,
+  gap: 30,
+  startAt: 0
+}).mount();
+
+
 
 import "./scss/style.scss";
+import "../node_modules/@glidejs/glide/dist/css/glide.core.min.css"
+import "../node_modules/@glidejs/glide/dist/css/glide.theme.min.css"
+
 
 ("use strict");
 
