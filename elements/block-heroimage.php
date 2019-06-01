7c5b1896beac5953f4380db7cf03e872ac1cd441
Bartłomiej Wilczek <bartlomiejwilczek@o2.pl>
2019-06-01 18:57:15 +0200

    heroimage on mobile

---
+++ elements/block-heroimage.php
@@ -0,0 +1,5 @@
+<section class="block-heroimage">
+ <div class="block-heroimage__content">
+     <h1>Nasza wiedza - twój spokój</h1>
+ </div>
+</section>
--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -5,10 +5,13 @@
 get_header();
 
 get_template_part('elements/block-slider');
+get_template_part('elements/block-heroimage');
 get_template_part('elements/block-solutions');
 get_template_part('elements/block-news');
 get_template_part('elements/block-case-studies');
+get_template_part('elements/block-tests');
 get_template_part('elements/block-logos-slider');
+get_template_part('elements/block-form-modal');
 ?>
 
 <?php get_footer();?>
